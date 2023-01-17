e555296b2ea0c241412d04a88313c8574c2dbcb1
Xury Greer <xury.greer@gmail.com>
2023-01-16 17:29:41 -0800

    Remove unnecessary import

--- bakery/__init__.py
+++ bakery/__init__.py
@@ -13,22 +13,15 @@ bl_info = {
 # More info: https://archive.blender.org/wiki/index.php/Dev:Py/Scripts/Cookbook/Code_snippets/Multi-File_packages/
 if "bpy" in locals():
 	import imp
-	imp.reload(caching_utilities)
 	imp.reload(baking_tools)
 
 else:
-	from . import caching_utilities
 	from . import baking_tools
 
-# Import general modules
-# import bpy
-
 def register():
-    # caching_utilities.register()
     baking_tools.register()
 
 def unregister():
-    # caching_utilities.unregister()
     baking_tools.unregister()
 
 if __name__ == "__main__":
